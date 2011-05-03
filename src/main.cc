b4ecb501281694ac5cf8560a07baa8c0a3a6ab29
Bastien Durel <bastien@durel.org>
2011-05-03 14:12:49 +0200

    skeleton

---
+++ src/main.cc
@@ -0,0 +1,56 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- */
+/*
+ * main.cc
+ * Copyright (C) Bastien Durel 2011 <bastien@durel.org>
+ * 
+ * NR-deckbuilder is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * NR-deckbuilder is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <gtkmm.h>
+#include <iostream>
+
+
+#ifdef ENABLE_NLS
+#  include <libintl.h>
+#endif
+
+
+/* For testing propose use the local (not installed) ui file */
+/* #define UI_FILE PACKAGE_DATA_DIR"/nr_deckbuilder/ui/nr_deckbuilder.ui" */
+#define UI_FILE "src/nr_deckbuilder.ui"
+   
+int
+main (int argc, char *argv[])
+{
+	Gtk::Main kit(argc, argv);
+	
+	//Load the Glade file and instiate its widgets:
+	Glib::RefPtr<Gtk::Builder> builder;
+	try
+	{
+		builder = Gtk::Builder::create_from_file(UI_FILE);
+	}
+	catch (const Glib::FileError & ex)
+	{
+		std::cerr << ex.what() << std::endl;
+		return 1;
+	}
+	Gtk::Window* main_win = 0;
+	builder->get_widget("main_window", main_win);
+	if (main_win)
+	{
+		kit.run(*main_win);
+	}
+	return 0;
+}
