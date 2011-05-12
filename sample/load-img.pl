e324cf79133eb744a92cf4570413088e7713baca
Bastien Durel <bastien@durel.org>
2011-05-12 20:14:39 +0200

    --delete option to replace an illustration

--- sample/load-img.pl
+++ sample/load-img.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/perl -w
+# -*- cperl -*-
 
 use DBI;
 use DBD::SQLite;
@@ -13,11 +14,13 @@ my $version = "Netrunner Limited";
 my $card = "Aujourd'Oui";
 my $img = '/tmp/netrunner-aujourd-oui.jpg';
 my $dbname = 'nr-full.db';
+my $delete = 0;
 
 my $result = GetOptions ("card=s" => \$card,
-												 "file=s" => \$img,
+												 "img|file=s" => \$img,
 												 "dbname=s" => \$dbname,
-												 "version=s" => \$version);
+												 "version=s" => \$version,
+												 "delete" => \$delete);
 
 
 my $dbargs = { AutoCommit => 0,
@@ -29,10 +32,18 @@ my $img_sth = $dbh->prepare($sql) or die $dbh->error;
 
 my $img_blob = read_file( $img, binmode => ':raw' ) or die "slurp $img: $!";
 
-$img_sth->bind_param(1, $card, SQL_VARCHAR) or die $dbh->error;;
-$img_sth->bind_param(2, $version, SQL_VARCHAR) or die $dbh->error;;
-$img_sth->bind_param(3, $img_blob, SQL_BLOB) or die $dbh->error;;
-$img_sth->execute() or die $dbh->error;;
+if ($delete) {
+	my $del_sth = $dbh->prepare("delete from illustration where ".
+															"card = ? and version = ?") or die $dbh->error;
+	$del_sth->bind_param(1, $card, SQL_VARCHAR) or die $dbh->error;
+	$del_sth->bind_param(2, $version, SQL_VARCHAR) or die $dbh->error;
+	$del_sth->execute() or die $dbh->error;
+}
+
+$img_sth->bind_param(1, $card, SQL_VARCHAR) or die $dbh->error;
+$img_sth->bind_param(2, $version, SQL_VARCHAR) or die $dbh->error;
+$img_sth->bind_param(3, $img_blob, SQL_BLOB) or die $dbh->error;
+$img_sth->execute() or die $dbh->error;
 
 $dbh->commit();
 $dbh->disconnect();
